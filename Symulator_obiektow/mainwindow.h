65b8fda3ebf348195e894b3cd19868da22c59f37
LeszczynskiK <leszczynskikrzysiu@gmail.com>
2025-02-24 14:24:07 +0100

    added resp by mouse press, firstyly chooose object type and then click mouse - will resp in mouse position

--- Symulator_obiektow/mainwindow.h
+++ Symulator_obiektow/mainwindow.h
@@ -20,20 +20,35 @@ private slots:
     void paintEvent(QPaintEvent *event);//background setter
     void exitApp();
     void backToMenu();//go back to 1st page
-    void respSquare();//your choise to resp object is..
-    void respRectangle();//your choise to resp object is..
-    void respCircle();//your choise to resp object is..
-    void respTriangle();//your choise to resp object is..
-    void respTrapeze();//your choise to resp object is..
+    void respSquare(int,int);//your choise to resp object is..
+    void respRectangle(int,int);//your choise to resp object is..
+    void respCircle(int,int);//your choise to resp object is..
+    void respTriangle(int,int);//your choise to resp object is..
+    void respTrapeze(int,int);//your choise to resp object is..
     void killSquare();//delete all of this object type
     void killRectangle();//delete all of this object type
     void killCircle();//delete all of this object type
     void killTriangle();//delete all of this object type
     void killTrapeze();//delete all of this object type
+    void mousePressEvent(QMouseEvent *event);//mouse event handling(here to resp object on click place)
 
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
+    //is object type button clicked(object selected? )
+    bool squareCondition=false;
+    bool rectangleCondition=false;
+    bool circleConditon=false;
+    bool triangleCondition=false;
+    bool trapezeCondition=false;
+
+    bool changeSquareCondition();
+    bool changeRectangleCondition();
+    bool changeCircleConditon();
+    bool changeTriangleCondition();
+    bool changeTrapezeCondition();
+
 private:
     QPixmap background;//Background txt
     QPushButton *exitAppButton;
